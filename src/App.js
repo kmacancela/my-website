ba5c279ec38bc1570ffa6b556ad7362125e5a1ad
Karina Macancela <karymacancela@gmail.com>
2020-01-16 10:56:45 -0500

    created header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withRouter } from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
+import Header from './components/Header'
+import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const sections = [
+  { title: 'About', url: '#' },
+  { title: 'Projects', url: '#' },
+  { title: 'Art', url: '#' },
+  { title: 'Blog', url: '#' },
+  { title: 'Contact', url: '#' },
+];
+
+const mainFeaturedPost = {
+  title: 'Learn more about what Karina Macancela is working on!',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Continue reading…',
+};
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header sections={sections} />
+      </Container>
+    </React.Fragment>
   );
 }
 
